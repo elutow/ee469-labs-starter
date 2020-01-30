7184291900f85c5c5abc856bbf4d04cedc05da4a
elutow <eloston@uw.edu>
2020-01-30 06:17:47 +0000

    debug_console.py: Move USB ID to separate constant

--- debug_console.py
+++ debug_console.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+
+import serial
 import tinyprog
 import usb
-import sys
 
 # Adjust this number to be the number of debug bytes
 DEBUGBYTES = 7
+# FPGA device USB ID
+USB_ID = '1d50:6130'
 
 def _align_serial_reads(port):
     """Ignore serial port values with 255 and align reads to DEBUGBYTES"""
@@ -61,7 +65,7 @@ def parse_cycle_output(cycle_count, cycle_output):
     pass
 
 def main():
-    ports = tinyprog.get_ports("1d50:6130")
+    ports = tinyprog.get_ports(USB_ID)
     print(f'Found {len(ports)} serial port(s)')
     if not ports:
         return
@@ -74,14 +78,16 @@ def main():
     write_loop = _write_loop(port)
     print('===BEGIN SERIAL OUTPUT===')
     with port:
-        while True:
-            next(write_loop)
-            ch = _align_serial_reads(port)
-            cycle_count, cycle_output = read_loop.send(ch)
-            parse_cycle_output(cycle_count, cycle_output)
+        try:
+            while True:
+                next(write_loop)
+                ch = _align_serial_reads(port)
+                cycle_count, cycle_output = read_loop.send(ch)
+                parse_cycle_output(cycle_count, cycle_output)
+        except KeyboardInterrupt:
+            print('Got KeyboardInterrupt. Exiting...')
+        except serial.serialutil.SerialException as exc:
+            print(f'ERROR: Serial connection threw error: {exc}')
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Got KeyboardInterrupt. Exiting...')
+    main()
